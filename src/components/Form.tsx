e595131992b80626086973b9cdba2ad1570f20cc
Gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-09-13 10:05:07 -0300

    feat: add function catch data

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export default function Form() {
+  const { register, handleSubmit } = useForm();
+  const [value, setValue] = useState("");
+
+  const createData = (data: any) => {
+    setValue(JSON.stringify(data, null, 2));
+    console.log(value);
+  };
+
   return (
     <>
       <div className="w-full flex justify-center items-center">
-        <div className="space-y-5 w-auto">
+        <form onSubmit={handleSubmit(createData)} className="space-y-5 w-auto">
           <div className="flex flex-col">
             <label className="text-purple-900 uppercase text-sm -tracking-wide">
               cardholder name
@@ -11,6 +22,7 @@ export default function Form() {
               type="text"
               placeholder="e.g. Jane Appleseed"
               className="border bg-zinc-100 shadow-sm"
+              {...register("name")}
             />
           </div>
           <div className="flex flex-col">
@@ -21,6 +33,7 @@ export default function Form() {
               type="text"
               placeholder="e.g. 1234 5678 9123 0000"
               className="border bg-zinc-100 shadow-sm"
+              {...register("card")}
             />
           </div>
           <div className="flex gap-5">
@@ -33,11 +46,13 @@ export default function Form() {
                   type="text"
                   placeholder="MM"
                   className="border bg-zinc-100 shadow-sm"
+                  {...register("month")}
                 />
                 <input
                   type="text"
                   placeholder="YY"
                   className="border bg-zinc-100 shadow-sm"
+                  {...register("year")}
                 />
               </div>
             </div>
@@ -49,6 +64,7 @@ export default function Form() {
                 type="text"
                 placeholder="e.g. 123"
                 className="border bg-zinc-100 shadow-sm"
+                {...register("cvv")}
               />
             </div>
           </div>
@@ -57,7 +73,7 @@ export default function Form() {
               Confirm
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
